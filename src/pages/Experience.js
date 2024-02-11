1102da235228fa8264dd22308b3a752861f75ab6
Dat Phan <datdoof@gmail.com>
2024-02-11 17:02:43 -0600

    fixed some experience info

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -29,7 +29,7 @@ function Experience() {
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            University of Minnesota Twin-Cities,   Minnesota
+            University of Minnesota Twin-Cities, Minnesota
           </h3>
 
           <h4 className="vertical-timeline-element-subtitle">
@@ -37,7 +37,27 @@ function Experience() {
           </h4>
 
           <p> Computer Science</p>
+          
         </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="Summer 2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Product Manager Intern, UnitedHealth Group
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Eden Prairie, MN
+          </h4>
+          <p>- Developed multiple tickets through <b>Jira</b> that assisted engineers in developing technical features and wrote <b>A/B
+            tests</b> that accelerated production by <b>3x</b> during product life cycle.</p>
+          <p>- Researched <b>100s</b> of data analytics by using <b>Adobe Analytics</b> and researching how well mobile onboarding did in
+              the past. This led to making informed decisions for engineers and business stakeholders.</p>
+          <p>- Provided input after researching data during meetings to discuss where the product prototype would go during
+            onboarding, which saved the company <b>4 dollars</b> per user who used the product feature with over <b>147 million</b> users who go through onboarding.</p>
+        </VerticalTimelineElement> 
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2023 - Present"
@@ -45,12 +65,17 @@ function Experience() {
           icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Software Engineer, Optum
+            Software Engineer, UnitedHealth Group
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
             Eden Prairie, MN
           </h4>
-          <p>Developed the backend infrastructure for 3 projects.</p>
+          <p>- Constructed a modern approach by integrating new UX onto the developer portal by developing a chatbot using a
+            <b>React</b> framework called <b>Botonic</b>.</p>
+          <p>- Developed new APIs in <b>Java SpringBoot</b> that accelerated existing production by <b>3x</b> by creating <b>SQL</b> queries to
+            retrieve data through <b>Oracle</b>.</p>
+          <p>- Followed company practices through CI/CD pipeline through <b>Jenkins</b> to make sure code follows company practices and to
+            deploy software to production.</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
